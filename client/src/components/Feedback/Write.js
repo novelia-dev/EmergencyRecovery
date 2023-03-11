e9b169e065e4d55b7d4bc063119c68ad99cb0c86
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-03-11 12:50:39 +0900

    WriteFeedback count

--- client/src/components/Feedback/Write.js
+++ client/src/components/Feedback/Write.js
@@ -31,6 +31,15 @@ const Stars = styled.div`
 function Choose(){
 
     const [Grade, setGrade] = useState([false,false, false, false, false])
+    const [good, setGood] = useState("");
+    const [bad, setBad] = useState("");
+
+    const handlegood = (e) => {
+        setGood(e.target.value);
+    }
+    const handlebad = (e) => {
+        setBad(e.target.value);
+    }
 
     const Click =() =>{
         alert('피드백 참여가 완료되었습니다.');
@@ -60,15 +69,17 @@ function Choose(){
             </div>
             <div>
                 <h5>좋았던 점</h5>
+                <h5>{good.length}자</h5>
             </div>
             <div>
-                <input style={{width:"588px", height:"382px"}} type="text" placeholder="300자 이상 입력해주세요. 작가분들께 직접 전달되는 피드백 입니다. 높임말을 사용해주세요. 욕설, 비난, 조롱의 경우 서비스 이용 제제 사유 입니다."></input>
+                <input style={{width:"588px", height:"382px"}} type="text" onChange={handlegood} placeholder="300자 이상 입력해주세요. 작가분들께 직접 전달되는 피드백 입니다. 높임말을 사용해주세요. 욕설, 비난, 조롱의 경우 서비스 이용 제제 사유 입니다."></input>
             </div>
             <div>
                 <h5>아쉬웠던 점</h5>
+                <h5>{bad.length}자</h5>
             </div>
             <div>
-                <input style={{width:"588px", height:"382px"}} type="text" placeholder="300자 이상 입력해주세요. 작가분들께 직접 전달되는 피드백 입니다. 높임말을 사용해주세요. 욕설, 비난, 조롱의 경우 서비스 이용 제제 사유 입니다."></input>
+                <input style={{width:"588px", height:"382px"}} type="text" onChange={handlebad} placeholder="300자 이상 입력해주세요. 작가분들께 직접 전달되는 피드백 입니다. 높임말을 사용해주세요. 욕설, 비난, 조롱의 경우 서비스 이용 제제 사유 입니다."></input>
             </div>
             <div>
                 <button onClick={Click}>제출하기</button>
