461fd923a66d66769418da16013574d247e48a5e
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-03-17 17:45:27 +0900

    probability fixed

--- client/src/components/Prize/Prize.js
+++ client/src/components/Prize/Prize.js
@@ -4,12 +4,12 @@ import image from '../Image/Starbucks.png';
 function getRandomPrize(){
     
     const prizes = [
-        {name: '뿌링클 20,000원', imageUrl: image ,chance:0.004 },
-        {name: '카카오페이지 캐시 5000원',imageUrl: image, chance: 0.026 },
-        {name: '스타벅스 아메리카노 Tall', imageUrl: image, chance: 0.05},
-        {name: '편의점 교환권 3000원', imageUrl: image,  chance: 0.11},
-        {name: '1500포인트', imageUrl: image, chance: 0.25 },
-        {name: '500포인트', imageUrl: image, chance: 0.56}
+        {name: '뿌링클 20,000원', imageUrl: image ,chance:0.01 },
+        {name: '카카오페이지 캐시 5000원',imageUrl: image, chance: 0.033 },
+        {name: '스타벅스 아메리카노 Tall', imageUrl: image, chance: 0.047},
+        {name: '편의점 교환권 3000원', imageUrl: image,  chance: 0.095},
+        {name: '1500포인트', imageUrl: image, chance: 0.205 },
+        {name: '0포인트', imageUrl: image, chance: 0.61}
     ];
     
     let rand = Math.random();
