527ae614a61f9ffae4ce70b299d3626ebb35e634
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-03-20 17:01:39 +0900

    mobilemyinput fixed

--- client/src/components/MyPage/mobile/MobileMyInput.js
+++ client/src/components/MyPage/mobile/MobileMyInput.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import MobileNavbar from '../../Navbar/MobileNavbar';
 import MobileFooter from '../../Footer/MobileFooter';
+import { getRandomPrize } from '../../Prize/Prize';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import image from '../../Image/Starbucks.png';
+import image1 from '../../Image/Ticket.png';
 
 function exchange(){
     if(window.confirm('정말 교환하시겠습니까?'))
@@ -12,6 +17,57 @@ function exchange(){
 }
 
 function MobileMyInput(){
+
+    const [prize, setPrize] = useState(null);
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const openModal = () => {
+      setModalVisible(true);
+    }
+
+    const closeModal = () => {
+      setModalVisible(false);
+    }
+
+    function handleClick(){
+      const randomPrize = getRandomPrize();
+      setPrize(randomPrize);
+    }
+
+    function Modal({className, onClose , maskClosable , closable , visible, children})
+{
+
+    const onMaskClick = (e) => {
+        if(e.target === e.currentTarget)
+        {
+            onClose(e)
+        }
+    }
+
+    const close = (e) => {
+        if(onClose){
+            onClose(e)
+        }
+    }
+
+    return(
+        <>
+            <ModalOverlay visible = {visible} />
+            <ModalWrapper className ={className} onClick={maskClosable ? onMaskClick : null} tabIndex="-1" visible={visible}>
+                <ModalInner tabIndex ="0" className = "modal-inner">
+                    {closable && <button style={{backgroundColor:"#FFFFFF",width:"30px", height:"30px", marginLeft:"500px"}} className="modal-close" onClick={close} >X</button>}
+                    {children}
+                </ModalInner>
+            </ModalWrapper>
+        </>
+    )
+}
+
+Modal.propTypes = {
+    visible: PropTypes.bool,
+}
+
+
     return(
         <div>
             <MobileNavbar />
@@ -20,25 +76,37 @@ function MobileMyInput(){
                <hr /> 
                <table>
                 <td><h5>8000 point</h5></td>
-                <td><button>경품 응모하기</button></td>
                </table>
                <hr />
                <h4>경품 응모하기</h4>
                <hr />
-               <img alt="경품" />
+               <img src={image1} style={{width:"300px", height:"159.99px"}} alt="경품" />
                <table>
                 <tr>
                 <td><h5>3월 2주차 경품 응모권 (3월 6일 발표)</h5></td>
-                <td></td>
-                </tr>
-                <tr>
-                 <td>
-                    <h5>bhc 황금올리브 기프티콘 1매</h5>
-                    <h5>문화상품권 1만원 3매</h5>
-                    <h5>스타벅스 아메리카노 기프티콘 5매</h5>
-                </td>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <td>
-                <h5><button onClick={exchange}>경품 응모권 교환하기</button></h5>    
+                <button onClick={openModal}>응모하기</button>
+                {
+                   modalVisible && <Modal
+                   visible={modalVisible}
+                   closable={true}
+                   maskClosable={true}
+                   onClose={closeModal}>
+                      <div>
+                        <h4>경품 응모 결과</h4>
+                        {prize && (
+                          <table>
+                          <tr><img src={image} /></tr>
+                          <tr>{prize.name}</tr>
+                         </table>
+                        )}
+                        <button onClick={handleClick}>확인하기</button>
+                        {console.log(prize)}
+                        
+                      </div>
+                   </Modal>
+                }   
                 </td>   
                 </tr>
                </table>
@@ -52,14 +120,11 @@ function MobileMyInput(){
              <div class="col-12 mt-3">
                 <div class="card">
                    <div class="img-square-wrapper" style={{marginTop:"25px"}}>
-                     <img alt="사진" />
+                     <img src={image}  alt="사진" />
                    </div>
                      <div class="card-body" >
                        <h4 class="card-title">스타벅스 아메리카노 기프티콘</h4>
-                       <p class="card-text">
-                        <h6>5000point</h6>
-                        <h6>남은 수량 : 100개</h6>
-                       </p>
+                       
                      </div>
                 </div>
              </div>
@@ -70,14 +135,11 @@ function MobileMyInput(){
              <div class="col-12 mt-3">
                 <div class="card">
                    <div class="img-square-wrapper" style={{marginTop:"25px"}}>
-                     <img alt="사진" />
+                     <img src={image} alt="사진" />
                    </div>
                      <div class="card-body" >
                        <h4 class="card-title">스타벅스 아메리카노 기프티콘</h4>
-                       <p class="card-text">
-                        <h6>5000point</h6>
-                        <h6>남은 수량 : 100개</h6>
-                       </p>
+                       
                      </div>
                 </div>
              </div>
@@ -91,5 +153,43 @@ function MobileMyInput(){
         </div>
     )
 }
+
+const ModalWrapper = styled.div`
+    box-sizing: border-box;
+    display: ${(props) => (props.visible ? 'block' : 'none')};
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 1000;
+    overflow: auto;
+    outline: 0;
+`;
+
+const ModalOverlay = styled.div`
+    box-sizing: border-box;
+    display: ${(props) => (props.visible ? 'block' : 'none')};
+    position: fixed;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-color: rgba(0,0,0,0.6);
+    z-index: 999;
+`;
+
+const ModalInner = styled.div`
+    box-sizing: border-box;
+    position: relative;
+    box-shadow: 0 0 6px 0 rgba(0,0,0,0.5);
+    background-color: #fff;
+    border-radius: 10px;
+    max-width: 600px;
+    top: 50%;
+    transform: translateY(-50%);
+    margin: 0 auto;
+    padding: 40px 20px;
+`;
 
 export default MobileMyInput;
