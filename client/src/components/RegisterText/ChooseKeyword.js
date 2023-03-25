de4b07c13a2d9b5bc17fff4a4c29e93b192f170d
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-03-25 13:55:54 +0900

    choosekeyword init

--- client/src/components/RegisterText/ChooseKeyword.js
+++ client/src/components/RegisterText/ChooseKeyword.js
@@ -1,5 +1,5 @@
 import React,{useState, useRef} from "react";
-
+import "./ChooseKeyword.css";
 
 function ChooseKeyword(){
 
@@ -15,11 +15,93 @@ function ChooseKeyword(){
     
     const [text3, setText3] = useState('');
     const [arr3, setarr3] = useState([]);
-    
+
     var size1 = arr.length;
     var size2 = brr.length;
     var size3 = arr2.length;
     var size4 = arr3.length;
+
+    const tagInput = document.querySelector(".tag-input");
+const tagArea = document.querySelector(".tag-area");
+const ul = document.querySelector(".tag-area ul");
+const label = document.querySelector(".label");
+
+let tags = [];
+
+function addEvent(element) {
+    tagArea.addEventListener("click", () => {
+        element.focus();
+    });
+
+    element.addEventListener("focus", () => {
+        tagArea.classList.add("active");
+        label.classList.add("label-active");
+    });
+
+    element.addEventListener("blur", (e) => {
+        tagArea.classList.remove("active");
+        if (element.value === "" && tags.length === 0) {
+            label.classList.remove("label-active");
+        }
+        if (!element.value.match(/^\s+$/gi) && element.value !== "") {
+            tags.push(e.target.value.trim());
+            element.value = "";
+            renderTags();
+        }
+    });
+
+    element.addEventListener("keydown", (e) => {
+        console.log(e);
+        const value = e.target.value;
+        if (
+            (e.keyCode === 32 ||
+                e.keyCode === 13 ||
+                value[value.length - 1] === " ") &&
+            !value.match(/^\s+$/gi) &&
+            value !== ""
+        ) {
+            tags.push(e.target.value.trim());
+            element.value = "";
+            renderTags();
+        }
+        if (e.keyCode === 8 && value === "") {
+            tags.pop();
+            renderTags();
+        }
+    });
+}
+addEvent(tagInput);
+
+function renderTags() {
+    ul.innerHTML = "";
+    tags.forEach((tag, index) => {
+        createTag(tag, index);
+    });
+    const input = document.createElement("input");
+    input.type = "text";
+    input.className = "tag-input";
+    addEvent(input);
+    ul.appendChild(input);
+    input.focus();
+    setTimeout(() => (input.value = ""), 0);
+}
+
+function createTag(tag, index) {
+    const li = document.createElement("li");
+    li.className = "tag";
+    const text = document.createTextNode(tag);
+    const span = document.createElement("span");
+    span.className = "cross";
+    span.dataset.index = index;
+    span.addEventListener("click", (e) => {
+        tags = tags.filter((_, index) => index != e.target.dataset.index);
+        renderTags();
+    });
+    li.appendChild(text);
+    li.appendChild(span);
+    ul.appendChild(li);
+}
+
 
     function inputok(){
 
@@ -127,7 +209,10 @@ function ChooseKeyword(){
         setText3('');
     }
 
+  
+    
 
+    
 
     return(
         <div>
@@ -179,6 +264,12 @@ function ChooseKeyword(){
                 <b>{arr3}</b>
             </div>
             <br />
+            <div class = "tag-area">
+                <label for="tag-input" class="label">Enter your favorite Languages</label>
+                <ul class="tags">
+                    <input type="text" class="tag-input" id="tag-input"></input>
+                </ul>
+            </div>
             <br />
             <div>
                 <button onClick={inputok}>다음</button>
