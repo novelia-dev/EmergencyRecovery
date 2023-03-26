ffdd972d8814ec80da2a36d015c20aa3255fda4b
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-03-26 16:18:36 +0900

    User Dropbox update

--- client/src/components/Dropdown/Ad.js
+++ client/src/components/Dropdown/Ad.js
@@ -5,6 +5,24 @@ import Mypage from '../Image/user.png';
 const Ad = props => {
     const [dropdownVisibility, setDropdownVisibility] = React.useState(false);
 
+    function movetomyinfo(){
+        window.location.href="/myinfo";
+    }
+
+    function movetotext(){
+        window.location.href="/text";
+    }
+
+    function movetoscrap(){
+        window.location.href="/myscrap";
+    }
+    function movetocoupon(){
+        window.location.href="/myinput";
+    }
+    function movetomyfeedback(){
+        window.location.href="/myfeedback";
+    }
+
     return (
         <div id='app' style={{zIndex:"0",marginTop:"28px"}}>
              <img className="Mypage" alt="Mypage" style={{width:"24px", height:"23px",marginLeft:"438px"}}src={Mypage} onClick={e => setDropdownVisibility(!dropdownVisibility)}/>
@@ -16,17 +34,16 @@ const Ad = props => {
                     <li style={{backgroundColor:"#666666",color:"white",fontSize:"22px",fontWeight:"400"}}>@@@@ point&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;포인트샵</li>
                     <br/>
                     <li><b>독자페이지</b></li>
-                    <li>스크랩한 작품</li>
-                    <li>피드백 참여 작품</li>
+                    <li onClick={movetoscrap}>스크랩한 작품</li>
+                    <li onClick={movetomyfeedback}>피드백 참여 작품</li>
                     <br/>
                     <li><b>작가페이지</b></li>
-                    <li>새글등록</li>
-                    <li>피드백 진행 중인 작품</li>
-                    <li>피드백 완료된 작품</li>
-                    <li>쿠폰함</li>
+                    <li onClick={movetotext}>새글등록</li>
+                    <li onClick={movetomyfeedback}>내 작품목록</li>
+                    <li onClick={movetocoupon}>쿠폰함</li>
                     <br/>
                     <li><b>기본정보</b></li>
-                    <li>기본정보 및 선호/금지태그 수정</li>
+                    <li onClick={movetomyinfo}>기본정보 및 선호/금지태그 수정</li>
                 </ul>
             </Dropdown>
         </div>
