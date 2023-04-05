5f0a8cdd9aaccc93531b2dacef7ccedb2368d0ea
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-04-05 23:23:52 +0900

    [Fix]kakaoRedirectURI

--- client/src/components/Loginpage/KakaoData.js
+++ client/src/components/Loginpage/KakaoData.js
@@ -1,4 +1,4 @@
 export const REST_API_KEY = "5e8fc3f0e711d0e38212b7ef2e42f246";
-export const REDIRECT_URI = "novelier.co.kr/login/callback";
+export const REDIRECT_URI = "https://novelier.co.kr/login/callback";
 export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 export const SECRET = 	"wyGSUMsZuJQQJqWfPs0HjLiaKKn0LA4q";
